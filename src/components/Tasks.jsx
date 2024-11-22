00e671b039ef5e3d30e77f2140e5a4731206ef71
Vivaldo Costa <110267396+vivaldo-costa@users.noreply.github.com>
2024-11-22 11:02:41 +0100

    Update Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -16,9 +16,10 @@ return (
 <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
 {tasks.map((task) => (
 <li key={task.id} className="flex gap-2" >
-        <button 
+           <button 
         onClick={() => onTaskClick(task.id)} 
-        className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted ? "line-through" : ""}`}>
+        className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${task.isCompleted ? "line-through" : ""}`}>
+        {task.isCompleted && <CheckIcon />}
         {task.title}
           </button>
 
