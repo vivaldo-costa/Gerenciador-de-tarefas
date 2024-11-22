241c2a6b2c5f3b3e52334dacd7d0e280d2cc1210
Vivaldo Costa <110267396+vivaldo-costa@users.noreply.github.com>
2024-11-22 10:15:01 +0100

    Update main.jsx
    Correção do "./pages/taskPage" para "./pages/TaskPage"

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import TaskPage from "./pages/taskPage";
+import TaskPage from "./pages/TaskPage";
 
 const router = createBrowserRouter([
   {
